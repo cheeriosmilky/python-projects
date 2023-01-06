59a0d289a97234a9e40c80b444aca3417716cd8b
cheeriosmilky <cheeriosmilky4@gmail.com>
2023-01-06 12:31:43 -0500

    uppp

--- api/book_api.py
+++ api/book_api.py
@@ -8,7 +8,6 @@ q = query.replace(' ', '%20')
 link = requests.get('https://www.googleapis.com/books/v1/volumes?q='+q)
 linkk = json.loads(link.text)
 items = linkk['items']
-info = linkk['items'][4]
 
 val = linkk['items'][1]['id']
 val2 = linkk['items'][2]['etag']
@@ -18,3 +17,10 @@ print('total items:', linkk['totalItems'])
 print('id:', val)
 print('etag:', val2)
 print('selflink:', val3)
+
+file = open(query, 'w')
+file.write(f"total items: {linkk['totalItems']}"+'\n')
+file.write(f"id: {val}"+'\n')
+file.write(f"etag: {val2}"+'\n')
+file.write(f"selflink: {val3}")
+file.close()
