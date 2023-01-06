a16ad48d91bdd887692f1815b0d52fa7ca5dbed5
cheeriosmilky <cheeriosmilky4@gmail.com>
2023-01-06 12:22:46 -0500

    d

--- api/book_api.py
+++ api/book_api.py
@@ -1,11 +1,20 @@
 import os
 import requests
-\
+import json
+
 query = input('book: ')
 q = query.replace(' ', '%20')
 
-link = 'https://www.googleapis.com/books/v1/volumes?q='+q
-linkk = requests.get(link)
+link = requests.get('https://www.googleapis.com/books/v1/volumes?q='+q)
+linkk = json.loads(link.text)
+items = linkk['items']
+info = linkk['items'][4]
 
-nums = 
-print(nums)
+val = linkk['items'][1]['id']
+val2 = linkk['items'][2]['etag']
+val3 = linkk['items'][3]['selfLink']
+
+print('total items:', linkk['totalItems'])
+print('id:', val)
+print('etag:', val2)
+print('selflink:', val3)
